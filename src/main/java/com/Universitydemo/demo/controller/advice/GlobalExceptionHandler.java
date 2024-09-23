07c34e99a93ef842530b98e6334f58b22c4f3a87
Kedar Nath Biswas <kedarnathbiswas06@gamil.com>
2024-09-23 15:08:45 +0600

    Second Commit

---
+++ src/main/java/com/Universitydemo/demo/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.Universitydemo.demo.controller.advice;
+
+import com.Universitydemo.demo.exception.CourseMaterialNotFoundException;
+import com.Universitydemo.demo.exception.CourseNotFoundException;
+import com.Universitydemo.demo.exception.StudentNotFoundException;
+import com.Universitydemo.demo.exception.TeacherNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CourseMaterialNotFoundException.class)
+    public ResponseEntity<String> handleCourseMaterialNotFoundException(CourseMaterialNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CourseNotFoundException.class)
+    public ResponseEntity<String> courseNotFound(CourseNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<String> handleStudentNotFound(StudentNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TeacherNotFoundException.class)
+    public ResponseEntity<String> handleTeacherNotFoundException(TeacherNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
